96bae4ea4e2cd22ee49649ca3e0b138efa8bbd25
xunxas0410 <ipk904904@gmail.com>
2020-10-24 11:48:15 +0800

    update 安裝好 composer npm

--- 3. Backend/backend/public/js/app.js
+++ 3. Backend/backend/public/js/app.js
@@ -49899,8 +49899,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\std\Desktop\Backend\backend\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\std\Desktop\Backend\backend\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ASUSer\Desktop\xunxas0410.github.io-master\Xpark\3. Backend\backend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ASUSer\Desktop\xunxas0410.github.io-master\Xpark\3. Backend\backend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
